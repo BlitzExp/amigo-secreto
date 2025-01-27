de913417a2f24496e499556a1d4b0e2a34dd4c71
Blitz <jsnajera1017@gmail.com>
2025-01-26 18:45:50 -0600

    app.js updated with all functionalities

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo(){
+    let amigo = document.getElementById('amigo').value;
+    if(amigo == ""){
+        alert("Por favor, inserta un nombre");
+    } else {
+        amigos.push(amigo);
+        document.getElementById('amigo').value = '';
+        mostrarAmigos();
+    }
+}
+
+function mostrarAmigos(){
+    let listaDeAmigos = document.querySelector('#listaAmigos');
+    listaDeAmigos.innerHTML = "";
+    for(let x = amigos.length-1; x >= 0; x--){
+        console.log("lol");
+        let li = document.createElement("li");
+        li.append(document.createTextNode(amigos[x]));
+        listaDeAmigos.append(li);
+    }
+}
+
+function sortearAmigo(){
+    if(amigos.length==0){
+        alert("Porfavor, introduce los nombres de los participantes");
+    }else{
+        let amigoSecreto = Math.floor(Math.random()*amigos.length);
+        document.getElementById('resultado').innerHTML = `El amigo secreto es ${amigos[amigoSecreto]}`
+        // condicionesIniciales();
+    }
+}
+
+// function condicionesIniciales(){
+//     amigos = [];
+//     document.querySelector('#listaAmigos').innerHTML = '';   
+// }
